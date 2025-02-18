798f9543bfe008afade34f3a6313dd7449e12c2a
jecm98 <155771280+jecm98@users.noreply.github.com>
2025-02-18 07:30:23 -0500

    Update dashboardapp.py

--- dashboardapp.py
+++ dashboardapp.py
@@ -7,7 +7,7 @@ import streamlit as st
 
 
 
-st.title("This is a title")
+st.title("Tabla Informativa")
 st.title("_Streamlit_ is :blue[cool] :sunglasses:")
 
 df = pd.read_csv("energy_sector_data_colombia.csv")
