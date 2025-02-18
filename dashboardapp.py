a6f27ff95a515aa37212f3d4f3e182c273a81751
jecm98 <155771280+jecm98@users.noreply.github.com>
2025-02-18 07:11:48 -0500

    Update dashboardapp.py

--- dashboardapp.py
+++ dashboardapp.py
@@ -1,4 +1,9 @@
-
+import pandas as pd # manejos de tablas
+import numpy as np # calculos numericos
+import matplotlib.pyplot as plt # constr. de graficos
+import seaborn as sns # libreria de graficos
+import scipy as sp # calculos complejos
+import streamlit as st
 
 
 
