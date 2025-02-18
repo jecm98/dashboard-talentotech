109debe55f20e53e42c81425be6d9794864f76d7
jecm98 <155771280+jecm98@users.noreply.github.com>
2025-02-18 07:06:41 -0500

    Update dashboardapp.py

--- dashboardapp.py
+++ dashboardapp.py
@@ -1,1 +1,8 @@
+import pandas as pd # manejos de tablas
+import numpy as np # calculos numericos
+import matplotlib.pyplot as plt # constr. de graficos
+import seaborn as sns # libreria de graficos
+import scipy as sp # calculos complejos
 
+df = pd.read_csv("energy_sector_colombia.csv")
+df
