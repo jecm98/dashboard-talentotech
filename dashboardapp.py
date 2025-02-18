cd1bbd0c5ca5793a73917f36610dac98bc0dc89c
jecm98 <155771280+jecm98@users.noreply.github.com>
2025-02-18 07:08:59 -0500

    Update dashboardapp.py

--- dashboardapp.py
+++ dashboardapp.py
@@ -3,6 +3,12 @@ import numpy as np # calculos numericos
 import matplotlib.pyplot as plt # constr. de graficos
 import seaborn as sns # libreria de graficos
 import scipy as sp # calculos complejos
+import streamlit as st
+
+
+
+st.title("This is a title")
+st.title("_Streamlit_ is :blue[cool] :sunglasses:")
 
 df = pd.read_csv("energy_sector_data_colombia.csv")
 df
